d0b355a65b55eed4f03113117c15e51f03bf2868
mzi <macchenjl@foxmail.com>
2019-03-18 16:06:58 +0800

    feat: use transition when page switching

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <transition
+    name="fade-in"
+    mode="out-in"
+  >
+    <router-view></router-view>
+  </transition>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./style/reset.less";
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: all 250ms cubic-bezier(0.55, 0, 0.1, 1);
+}
+.fade-in-enter,
+.fade-in-leave-active {
+  opacity: 0;
 }
 </style>
