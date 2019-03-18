2785e4312826b164c4191c845833b2f7518bd21b
mzi <macchenjl@foxmail.com>
2019-03-18 16:08:26 +0800

    feat: bind font-awesome svg, msgbox && confirm components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimesCircle, faPlus, faCheck, faTrash, faChevronLeft, faSpinner, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import './lib/remFlex'
+
+import MsgBox from './components/MessageBox'
+import Confirm from './components/Confirm'
+
+library.add(faTimesCircle, faPlus, faCheck, faTrash, faChevronLeft, faSpinner, faExclamationTriangle)
+
+Vue.component('fa-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$msg = MsgBox
+Vue.prototype.$confirm = Confirm
 
 new Vue({
   router,
